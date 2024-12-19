e7ec4e29c8dec4febbbc4fdd30dbd9f0affc0cc9
Serhat GENÇ <gncsrht05@gmail.com>
2024-12-19 14:47:05 +0300

    şakaSitesi

---
+++ saka-sitesi/script.js
@@ -0,0 +1,18 @@
+document.getElementById('btnKenevir').addEventListener('click', function() {
+    // Kenevir hakkında Wikipedia sayfasına yönlendir
+    window.open('https://tr.wikipedia.org/wiki/Kenevir', '_blank');
+});
+
+document.getElementById('btnBiletler').addEventListener('click', function() {
+    // Uçak bileti araması yapmak için örnek bir uçak bileti sitesine yönlendir
+    window.open('https://www.enuygun.com/ucak-bileti/arama/istanbul-amsterdam-ista-amsa/?gidis=01.07.2025&yetiskin=1&sinif=economy&save=1&geotrip=international&trip=international&ref=ft-homepage', '_blank');
+});
+
+document.getElementById('btnRedLine').addEventListener('click', function() {
+    // Google Görseller'de Red Line araması yapmak için yönlendirme
+    window.open('https://www.google.com/search?q=hollanda+red+light&sca_esv=c818ec8d0c4189e3&udm=2&biw=1536&bih=738&sxsrf=ADLYWIIo4xq79XtOlaIZBikX-UWwzfE_kQ%3A1734608619062&ei=6wZkZ5GwA7aTxc8Pvt7O4Qk&ved=0ahUKEwjR0cDU4LOKAxW2SfEDHT6vM5wQ4dUDCBE&uact=5&oq=hollanda+red+light&gs_lp=EgNpbWciEmhvbGxhbmRhIHJlZCBsaWdodDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIGEAAYCBgeSKgOUJcFWKoLcAF4AJABAJgB9wGgAfAHqgEFMC4zLjK4AQPIAQD4AQGYAgWgAvsGwgIEECMYJ8ICBBAAGB6YAwCIBgGSBwUxLjIuMqAH8R0&sclient=img', '_blank');
+});
+document.getElementById('btnSpecial').addEventListener('click', function() {
+    // Google Görseller'de Red Line araması yapmak için yönlendirme
+    window.open('https://www.youtube.com/watch?v=8u2pI5g_f7c', '_blank');
+});
